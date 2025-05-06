6a9264d8389494d08151600164b13d0da13e5265
Snowmuffin <77647785+snowmuffin@users.noreply.github.com>
2025-05-06 18:36:40 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   trailingSlash: false,
 
   basePath: '',
